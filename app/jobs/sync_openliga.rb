48c4dae7fda92172d5119ea719220822ba0ae9f6
Jan Schwenzien <jan@general-scripting.com>
2014-07-08 22:14:09 +0200

    1/2 finals

--- app/jobs/sync_openliga.rb
+++ app/jobs/sync_openliga.rb
@@ -16,7 +16,7 @@ class SyncOpenliga
       require 'openligadb'
       liga = OpenLigaDB.new
       response = liga.request('matchdata_by_group_league_saison',
-                                groupOrderId: 5,
+                                groupOrderId: 4,
                                 leagueSaison: 2014,
                                 leagueShortcut: 'WM-2014')
       I18n.locale = :de
